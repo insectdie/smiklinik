a78c4af15dd8bf8d18bcf722d4a9b38e78eb1abb
Daniel Sinaga <nieljrnaga@gmail.com>
2023-09-27 16:44:45 +0700

    update

--- public/assets/script/StatusKeluarga.js
+++ public/assets/script/StatusKeluarga.js
@@ -1,4 +1,5 @@
 var table;
+var edit;
 
 function load() {
     table = $('#example').dataTable( {
@@ -17,7 +18,7 @@ function load() {
         fnCreatedRow: function (nRow, mData, iDataIndex) {
             $('td:eq(3)', nRow).html('');
             var row3 = $('td:eq(3)', nRow).html();
-            row3 += "<button type='button' id='"+mData.id+"' class='btn btn-label-secondary waves-effect'><i class='fa fa-edit'></i></button>";
+            row3 += "<button type='button' id='bt-edit' data-id='"+mData.id+"' class='btn btn-label-secondary waves-effect'><i class='fa fa-edit'></i></button>";
             row3 += "<button type='button' id='bt-delete' data-id='"+mData.id+"' class='btn btn-label-secondary waves-effect'><i class='fa fa-trash'></i></button>";
             $('td:eq(3)', nRow).html(row3);  
         }
@@ -32,7 +33,9 @@ $(document).ready(function() {
 
     load();
 
-    $( "#bt-save" ).on( "click", function() {
+    edit = false;
+
+    $( "#bt-save" ).on( "click", function() {   
         event.preventDefault();
         var data = JSON.stringify({
             statusKeluarga: $('#ed_status_keluarga').val(),
@@ -57,6 +60,7 @@ $(document).ready(function() {
                 });
                 $('#example').DataTable().ajax.reload();
                 clear();
+                edit = false;
             },
             error : function(){
                 Swal.fire({
@@ -69,9 +73,11 @@ $(document).ready(function() {
     
     } );
 
-    $("#bt-delete").on( "click", function() {
+    $('#example').on('click', '#bt-delete', function(){
+        let id = $(this).attr('data-id');
+
         $.ajax({
-            url :  'http://10.10.117.159:5188/api/status-keluarga/'+$(),
+            url :  'http://10.10.117.159:5188/api/status-keluarga',
             type: "POST",
             data: data,
             headers: {
@@ -82,65 +88,49 @@ $(document).ready(function() {
             success: function(res){
                 Swal.fire({
                     icon: 'success',
-                    title: 'Data berhasil di delete!',
+                    title: 'Data berhasil di simpan!',
                     showConfirmButton: false
                 });
                 $('#example').DataTable().ajax.reload();
                 clear();
+                edit = false;
             },
             error : function(){
                 Swal.fire({
                     icon: 'error',
-                    title: 'Gagal delete data!',
+                    title: 'Gagal simpan data!',
                     showConfirmButton: false
                 });
             } 
         });
     });
 
-    $('#example').on('click', '#bt-delete', function(){
+    $('#example').on('click', '#bt-edit', function(){
         let id = $(this).attr('data-id');
+        // event.preventDefault();
+        // var data = JSON.stringify({
+        //     statusKeluarga: $('#ed_status_keluarga').val(),
+        //     status: $('input[name="ed_status"]:checked').val(),
+        //     userCreated: "andry"
+        // })
 
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            // showCancelButton: false, // There won't be any cancel button
-            // showConfirmButton: false,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-          }).then((result) => {
-            if (result.value) {
-                $.ajax({
-                    url :  'http://10.10.117.159:5188/api/status-keluarga/'+id,
-                    type: "DELETE",
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    dataType : 'json',
-                    success: function(res){
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Data berhasil di delete!',
-                            showConfirmButton: false
-                        });
-                        $('#example').DataTable().ajax.reload();
-                        clear();
-                    },
-                    error : function(){
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Gagal delete data!',
-                            showConfirmButton: false
-                        });
-                    } 
-                });
-            }
-          })
+        $('#ed_status_keluarga').val()
+        $.ajax({
+            url :  'http://10.10.117.159:5188/api/status-keluarga/'+id,
+            type: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            dataType : 'json',
+            success: function(res){
+                $('#ed_status_keluarga').val(res.statusKeluarga),
+                $('#ed_status_keluarga').val(res.statusKeluarga)
+            },
+            error : function(){
+            } 
+        });
 
-        
     });
 
 });
